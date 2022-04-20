1e232de86b6ff6fcea9390abc798f8b327528a9c
Silas <silasroot321@gmail.com>
2022-04-20 16:09:24 -0300

    Adding setup and installation requirements

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+DISTNAME = 'Files Tools'
+DESCRIPTION = 'Set of scripts for automating tasks'
+LONG_DESCRIPTION = open('README.md', encoding="utf8").read()
+MAINTAINER = 'Silas João'
+URL = 'https://github.com/silasabs/FilesTool'
+LICENSE = 'MIT'
+
+
+def requirements(file):
+    lineiter = (line.strip() for line in open(file))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+files = ["scripts/*"]
+
+setup(
+    name=DISTNAME,
+    maintainer=MAINTAINER,
+    description=DESCRIPTION,
+    license=LICENSE,
+    url=URL,
+    install_requires=requirements("./requirements.txt"),
+    long_description=LONG_DESCRIPTION,
+    packages=['scripts'],
+    package_data={'scripts': files},
+    scripts=["runner"],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    classifiers=[
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    python_requires='>=3.9',
+)
